6a9d4e5b330863c533fe02e18f64cf6b91bab2dc
Renaud Bouckenooghe <iraadit@gmail.com>
2018-07-15 00:29:52 +0200

    Walk-through code

--- main.py
+++ main.py
@@ -17,6 +17,19 @@ if not tf.test.gpu_device_name():
 else:
     print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
 
+L2_REG = 1e-3 #1e-5
+STDEV = 1e-3
+KEEP_PROB = 0.5
+LEARNING_RATE = 1e-4 # 0.0009
+EPOCHS = 20
+BATCH_SIZE = 5
+
+IMAGE_SHAPE = (160, 576)
+NUM_CLASSES = 2
+
+DATA_DIR = './data'
+RUNS_DIR = './runs'
+MODEL_DIR = './models_3col'
 
 def load_vgg(sess, vgg_path):
     """
@@ -25,7 +38,6 @@ def load_vgg(sess, vgg_path):
     :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
     :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
     """
-    # TODO: Implement function
     #   Use tf.saved_model.loader.load to load the model and weights
     vgg_tag = 'vgg16'
     vgg_input_tensor_name = 'image_input:0'
@@ -33,8 +45,16 @@ def load_vgg(sess, vgg_path):
     vgg_layer3_out_tensor_name = 'layer3_out:0'
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
-    
-    return None, None, None, None, None
+
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+
+    image_input = tf.get_default_graph().get_tensor_by_name(vgg_input_tensor_name)
+    keep_prob = tf.get_default_graph().get_tensor_by_name(vgg_keep_prob_tensor_name)
+    layer3_out = tf.get_default_graph().get_tensor_by_name(vgg_layer3_out_tensor_name)
+    layer4_out = tf.get_default_graph().get_tensor_by_name(vgg_layer4_out_tensor_name)
+    layer7_out = tf.get_default_graph().get_tensor_by_name(vgg_layer7_out_tensor_name)
+
+    return image_input, keep_prob, layer3_out, layer4_out, layer7_out
 tests.test_load_vgg(load_vgg, tf)
 
 
@@ -47,8 +67,34 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :param num_classes: Number of classes to classify
     :return: The Tensor for the last layer of output
     """
-    # TODO: Implement function
-    return None
+    # 1x1 convolution of vgg layer7
+    layer7_out = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, padding='same',
+                                kernel_regularizer=tf.contrib.layers.l2_regularizer(L2_REG))
+    # upsample
+    layer4_in_1 = tf.layers.conv2d_transpose(layer7_out, num_classes, 4, 2, padding='same',
+                                kernel_regularizer=tf.contrib.layers.l2_regularizer(L2_REG))
+    # tf.Print(layer4_in_1, [tf.shape(layer4_in_1)[1:3]]) # output shape
+    # 1x1 convolution of vgg layer4
+    layer4_in_2 = tf.layers.conv2d(vgg_layer4_out, num_classes, 1, padding='same',
+                                kernel_regularizer=tf.contrib.layers.l2_regularizer(L2_REG))
+    # skip connection (element-wise addition)
+    layer4_out = tf.add(layer4_in_1, layer4_in_2)
+
+    # upsample
+    layer3_in_1 = tf.layers.conv2d_transpose(layer4_out, num_classes, 4, 2, padding='same',
+                                kernel_regularizer=tf.contrib.layers.l2_regularizer(L2_REG))
+    # 1x1 convolution of vgg layer4
+    layer3_in_2 = tf.layers.conv2d(vgg_layer3_out, num_classes, 1, padding='same',
+                                kernel_regularizer=tf.contrib.layers.l2_regularizer(L2_REG))
+
+    # skip connection (element-wise addition)
+    layer3_out = tf.add(layer3_in_1, layer3_in_2)
+
+    # upsample
+    nn_last_layer = tf.layers.conv2d_transpose(output, num_classes, 16, 8, padding='same',
+                                kernel_regularizer=tf.contrib.layers.l2_regularizer(L2_REG))
+
+    return nn_last_layer
 tests.test_layers(layers)
 
 
@@ -61,8 +107,16 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :param num_classes: Number of classes to classify
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
-    # TODO: Implement function
-    return None, None, None
+    # make logits a 2D tensor where each row represents a pixel and each column a class
+    logits = tf.reshape(nn_last_layer, (-1, num_classes)) # not needed
+    correct_label = tf.reshape(correct_label, (-1,num_classes))
+    # define loss function
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, correct_label))
+    # define training operation
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+    train_op = optimizer.minimize(cross_entropy_loss)
+
+    return logits, train_op, cross_entropy_loss
 tests.test_optimize(optimize)
 
 
@@ -81,16 +135,20 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param keep_prob: TF Placeholder for dropout keep probability
     :param learning_rate: TF Placeholder for learning rate
     """
-    # TODO: Implement function
-    pass
+    sess.run(tf.global_variables_initializer())
+
+    print("Training for {} epochs".format(epochs))
+    for epoch in range(epochs):
+        print("Epoch : {}".format(epoch + 1))
+        for image, label in get_batches_fn(batch_size):
+             _, loss = sess.run([train_op, cross_entropy_loss],
+                               feed_dict={input_image: image, correct_label: label, keep_prob: KEEP_PROB, learning_rate: LEARNING_RATE}) #1e-4
+            print("Loss: = {:.3f}".format(loss))
+        print()
 tests.test_train_nn(train_nn)
 
 
 def run():
-    num_classes = 2
-    image_shape = (160, 576)
-    data_dir = './data'
-    runs_dir = './runs'
     tests.test_for_kitti_dataset(data_dir)
 
     # Download pretrained vgg model
@@ -102,21 +160,50 @@ def run():
 
     with tf.Session() as sess:
         # Path to vgg model
-        vgg_path = os.path.join(data_dir, 'vgg')
+        vgg_path = os.path.join(DATA_DIR, 'vgg')
         # Create function to get batches
-        get_batches_fn = helper.gen_batch_function(os.path.join(data_dir, 'data_road/training'), image_shape)
+        get_batches_fn = helper.gen_batch_function(os.path.join(DATA_DIR, 'data_road/training'), IMAGE_SHAPE)
 
         # OPTIONAL: Augment Images for better results
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
-        # TODO: Build NN using load_vgg, layers, and optimize function
+        # Build NN using load_vgg, layers, and optimize function
+        # Tensorflow placeholders
+        correct_label = tf.placeholder(tf.int32, [None, None, None, NUM_CLASSES], name='correct_label')
+        learning_rate = tf.placeholder(tf.float32, name='learning_rate')
 
-        # TODO: Train NN using the train_nn function
+        # Getting layers from VGG
+        input_image, keep_prob, layer3_out, layer4_out, layer7_out = load_vgg(sess, vgg_path)
 
-        # TODO: Save inference data using helper.save_inference_samples
-        #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+        # Creating the rest of the layers for a FCN
+        nn_last_layer = layers(layer3_out, layer4_out, layer7_out, num_classes)
+
+        # Create outputs, training operation and loss
+        logits, train_op, cross_entropy_loss = optimize(nn_last_layer, correct_label, learning_rate, num_classes)
+
+        # Instentiation of the saver
+        saver = tf.train.Saver()
+
+        # Restore saved model
+        # saver.restore(sess, './runs/sem_seg_model.ckpt') # RUNS_DIR
+
+
+        # Train NN using the train_nn function
+        train_nn(sess, EPOCHS, BATCH_SIZE, get_batches_fn, train_op, cross_entropy_loss, input_image,
+                     correct_label, keep_prob, learning_rate)
+
+        # Save model
+        #run_time = str(time.time())
+        #model_out_dir = os.path.join(model_dir, run_time)
+        #saver.save(sess, os.path.join(model_out_dir, 'model'))
+        saver.save(sess, './runs/sem_seg_model.ckpt') # RUNS_DIR
+
+        # Save inference data using helper.save_inference_samples
+        helper.save_inference_samples(RUNS_DIR, DATA_DIR, sess, image_shape, logits, keep_prob, input_image)
 
         # OPTIONAL: Apply the trained model to a video
+        # data_sub_dir = 'project_video'
+        # helper.save_to_clip(data_sub_dir, DATA_DIR, sess, image_shape, logits, keep_prob, input_image)
 
 
 if __name__ == '__main__':
